407d6ddf7020f7f4cd801633fc2c4ca3cfff319f
loginreward <nolan.lessard.music@gmail.com>
2026-06-21 12:54:22 -0400

    chore: basic web server

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+func renderTemplate(w http.ResponseWriter, name string) {
+	tmplPath := filepath.Join("html", name+".html")
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Template parsing error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Template execution error: "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "index")
+	})
+
+	fs := http.FileServer(http.Dir("./imports"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	fmt.Println("Server listening on :4040")
+	http.ListenAndServe(":4040", mux)
+}
